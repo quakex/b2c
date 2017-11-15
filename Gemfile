60fb6458bf650a51667f6358035bb0cfab7a559a
quakex <iquakex@gmail.com>
2017-11-15 11:09:58 +0800

    dev 09

--- Gemfile
+++ Gemfile
@@ -33,6 +33,7 @@ gem 'bootstrap-sass'
 gem 'sorcery'
 gem 'font-awesome-rails'
 gem 'ancestry'
+gem 'will_paginate'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,7 +1,9 @@
 class Admin::CategoriesController < Admin::BaseController
   def index
+    @categories = Category.roots.page(params[:page] || 1).per_page(params[:per_page] || 10).order(id: "desc")
   end
 
   def new
+    @category = Category.new
   end
 end
